a3559d19bad8f2857823906e71d6ef175703bd2b
mp02 <molly_patterson@yahoo.com>
2019-07-08 22:36:44 -0500

    finalized project, deleted additional comments, added win and lose alert

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # unit-4-game
+
+a random number is generated in the green box.
+
+user clicks on the different crystals to get different random numbers to try to add up to the random generated number in the green box. if you match the number, you win! if you go over, you lose.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,14 @@
 $(document).ready(function() {
 
-var targetNumber = 20
-//Math.floor(Math.random()*101 + 19);
+var targetNumber = Math.floor(Math.random()*101 + 19);
 var counter = 0;
 var wins = 0;
 var losses = 0;
-// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-//Math.round(Math.random()*11 + 1);
-//var increment = numberOptions[Math.round(Math.random())];
 
 function setupCats (){
     var numberOptions = Math.round(Math.random()*11 + 1);
     return numberOptions;
-    //return 
-    
 }
-
-
 
 $("#generatednumber").text(targetNumber);
 
@@ -28,18 +20,12 @@ console.log(Crystal1);
 console.log(Crystal2);
 console.log(Crystal3);
 console.log(Crystal4);
-// how to get all crystals a diff random number?
-
-
-//create var for images so they all go through this function
-// give all images a random number
 
 $("#crystal-image-1").on("click", function (){
     counter+= Crystal1;
     $("#guessesnumber").text(counter);
     winslosses();
-    // alert("test complete" + counter);
-    //console.log(targetNumber);
+
 })
 
 $("#crystal-image-2").on("click", function(){
@@ -65,40 +51,29 @@ function winslosses (){
     if (counter === targetNumber) {
         wins++;
         $("#wins").text("wins: " + wins);
-        //  alert("winnning");
+        alert("you win!");
         reset();
     }
     
     else if (counter > targetNumber) {
-        //alert("you lose");
         losses++;
         $("#losses").text("losses: " + losses);
+        alert("you lose");
         reset();
     }
-    //else {
-        //   alert("looseee");
-        // }
-        
-        $("#image1").on("click", )
-        
+    
     }
     
    function reset() {
         counter = 0;
         $("#guessesnumber").text(0); 
+        targetNumber = Math.floor(Math.random()*101 + 19);
+        $("#generatednumber").text(targetNumber);
         Crystal1 = setupCats();
         Crystal2 = setupCats();
         Crystal3 = setupCats();
         Crystal4 = setupCats();
-        // repeat for all crystals, same as var. new generated number
-        // reset - targetNumber = 
-        //reset to zero??? not just start next number
-    
-    //new generated number
-    //new numbers for each crystal
+
     }
     
-})
-
-//* The random number shown at the start of the game should be between 19 - 120.
-//* Each crystal should have a random hidden value between 1 - 12.
+});
